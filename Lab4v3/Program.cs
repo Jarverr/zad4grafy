ec61e10a6505b9613649089f31dff821b403c055
Michal <KrzyzowskiM1998@gmail.com>
2020-11-20 22:03:20 +0100

    rebuild version

--- Lab4v3/Program.cs
+++ Lab4v3/Program.cs
@@ -15,16 +15,16 @@ namespace Lab4v3
             //var text = "";//Console.ReadLine();
             //using StreamReader sr = new StreamReader("text.txt");
             //args[0] = sr.ReadToEnd();
-
-            args[0] = "abcxxxabc";
+            //Console.WriteLine(args[0].Length);
+            args[0] = "ababcababcac";
             Stopwatch sw = new Stopwatch();
             sw.Start();
             var trie = new UkkonenTrie<int>(1);
             //var trie2 = new SuffixTrie<int>(1);
             trie.Add(args[0], 1);
-            
-            Console.WriteLine("tree has been build (in {0})",sw.Elapsed);
-           
+
+            Console.WriteLine("tree has been build (in {0})", sw.Elapsed);
+
             string temp = "";
             List<string> myLongestWordEdge = new List<string>();
             //string[] temp = new string[trie._root._edges.Count];
@@ -39,63 +39,69 @@ namespace Lab4v3
             //    myLongestWordEdge.Add(myLongestWord);
             //}
             //);
-        //}
-        //catch (AggregateException ae)
-        //{
-        //    var ignoredExceptions = new List<Exception>();
-        //    // This is where you can choose which exceptions to handle.
-        //    foreach (var ex in ae.Flatten().InnerExceptions)
-        //    {
-        //        if (ex is ArgumentException)
-        //            Console.WriteLine(ex.Message);
-        //        else
-        //            ignoredExceptions.Add(ex);
-        //    }
-        //    if (ignoredExceptions.Count > 0) throw new AggregateException(ignoredExceptions);
-        //}
+            //}
+            //catch (AggregateException ae)
+            //{
+            //    var ignoredExceptions = new List<Exception>();
+            //    // This is where you can choose which exceptions to handle.
+            //    foreach (var ex in ae.Flatten().InnerExceptions)
+            //    {
+            //        if (ex is ArgumentException)
+            //            Console.WriteLine(ex.Message);
+            //        else
+            //            ignoredExceptions.Add(ex);
+            //    }
+            //    if (ignoredExceptions.Count > 0) throw new AggregateException(ignoredExceptions);
+            //}
 
-        temp = string.Empty;
+            temp = string.Empty;
             for (int i = 0; i < myLongestWordEdge.Count; i++)
             {
-                if (temp.Length<myLongestWordEdge[i].Length)
+                if (temp.Length < myLongestWordEdge[i].Length)
                 {
                     temp = myLongestWordEdge[i];
                 }
             }
             string myLongestWord = "";
-            TraversePostOrder(trie._root._edges, ref myLongestWord, ref temp, args[0]);
-            sw.Stop(); 
-            Console.WriteLine("String: "+ myLongestWord + " ; "+ myLongestWord.Length*2+$"\nTime: {sw.Elapsed}");
+            foreach (var item in trie._root._edges)
+            {
+                temp = item.Value.Label;
+                TraversePostOrder(item, ref myLongestWord, ref temp, args[0]);
+                break;
+
+            }
+            sw.Stop();
+            Console.WriteLine("String: " + myLongestWord + " ; " + myLongestWord.Length * 2 + $"\nTime: {sw.Elapsed}");
 
 
             //trie2.Add(text,1);
         }
         static string toRewrite = "";
         static bool isOkay = false;
-        public static void TraversePostOrder(IDictionary<char, Edge<int>> edges, ref string theLongestRepreatedPrefix, ref string currentWord, string text)
+        public static void TraversePostOrder(KeyValuePair<char, Edge<int>> edges, ref string theLongestRepreatedPrefix, ref string currentWord, string text)
         {
-            if (edges.Count != 0)
+            if (edges.Value.Target._edges.Count != 0)
             {
-
                 if (theLongestRepreatedPrefix.Length < currentWord.Length)
                 {
-                    (isOkay, toRewrite) = getTheLongestRepeatedString(  theLongestRepreatedPrefix,  currentWord,  text);
+                    (isOkay, toRewrite) = getTheLongestRepeatedString(theLongestRepreatedPrefix, currentWord, text);
                     if (isOkay)
                     {
                         theLongestRepreatedPrefix = toRewrite;
                     }
 
                 }
-                foreach (var item in edges)
+                foreach (var item in edges.Value.Target._edges)
                 {
                     currentWord += item.Value.Label;
-                    TraversePostOrder(item.Value.Target._edges, ref theLongestRepreatedPrefix, ref currentWord,text);
+                    TraversePostOrder(item, ref theLongestRepreatedPrefix, ref currentWord, text);
                     currentWord = currentWord.Substring(0, (currentWord.Length - item.Value.Label.Length));
+                    break;
                 }
             }
         }
 
-        private static (bool,string) getTheLongestRepeatedString( string theLongestRepreatedPrefix,  string currentWord, string text)
+        private static (bool, string) getTheLongestRepeatedString(string theLongestRepreatedPrefix, string currentWord, string text)
         {
             string temp = "";
             if (text.IndexOf(currentWord) > -1)
@@ -106,7 +112,7 @@ namespace Lab4v3
             {
                 return (false, currentWord);
             }
-            if (temp.Contains(currentWord)&&text.Substring((text.IndexOf(currentWord)+currentWord.Length),currentWord.Length)==currentWord)
+            if (temp.Contains(currentWord) && text.Substring((text.IndexOf(currentWord) + currentWord.Length), currentWord.Length) == currentWord)
             {
                 if (currentWord.Length > 0)
                     return (true, currentWord);
@@ -115,13 +121,13 @@ namespace Lab4v3
             }
             else
             {
-                if (currentWord.Length>0)
+                if (currentWord.Length > 0)
                 {
                     currentWord = currentWord.Substring(0, currentWord.Length - 1);
-                    (isOkay,currentWord)=getTheLongestRepeatedString(theLongestRepreatedPrefix, currentWord, text);
+                    (isOkay, currentWord) = getTheLongestRepeatedString(theLongestRepreatedPrefix, currentWord, text);
                 }
-                
-                return (isOkay,currentWord);
+
+                return (isOkay, currentWord);
             }
         }
     }
